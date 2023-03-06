15392502e76908c6d39efda496269d693915a8c7
zero-ljz <2267719005@qq.com>
2023-03-06 16:48:17 +0800

    优化models中多对多关系代码，准备应用到数据库

--- bbs/admin.py
+++ bbs/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Follow, Like, Log, Message, Notification, Option, Permission, Post, PostsTags, Role, RolesPermissions, Tag, User
+from .models import Collect, Comment, Follow, Like, Log, Message, Notification, Option, Permission, Post, Role, Tag, User
 
+admin.site.register(Collect)
+admin.site.register(Comment)
 admin.site.register(Follow)
 admin.site.register(Like)
 admin.site.register(Log)
@@ -12,8 +14,6 @@ admin.site.register(Notification)
 admin.site.register(Option)
 admin.site.register(Permission)
 admin.site.register(Post)
-admin.site.register(PostsTags)
 admin.site.register(Role)
-admin.site.register(RolesPermissions)
 admin.site.register(Tag)
 admin.site.register(User)
--- bbs/models.py
+++ bbs/models.py
@@ -5,13 +5,22 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = True` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+
+# 这是一个自动生成的Django模型模块。
+# 你必须手动执行以下操作来清理:
+# *重新排列模型的顺序
+# *确保每个模型都有一个字段primary_key=True
+# *确保每个ForeignKey和OneToOneField都有“on_delete”设置为所需的行为
+# *如果你想让Django创建，修改和删除表，删除' managed = True '行
+# 可以重命名模型，但不要重命名db_table的值或字段名。
+
 from django.db import models
 
 
 class Collect(models.Model):
     collector = models.ForeignKey('User', models.DO_NOTHING)
     collected = models.ForeignKey('Post', models.DO_NOTHING)
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     class Meta:
         managed = True
@@ -21,12 +30,15 @@ class Collect(models.Model):
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
-    created = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     reviewed = models.BooleanField(blank=True, null=True)
     post = models.ForeignKey('Post', models.DO_NOTHING, blank=True, null=True)
     author = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
     replied = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)
 
+    def __str__(self):
+        return self.content
+    
     class Meta:
         managed = True
         db_table = 'comment'
@@ -35,7 +47,7 @@ class Comment(models.Model):
 class Follow(models.Model):
     follower = models.ForeignKey('User', models.DO_NOTHING, related_name='follower_user')
     followed = models.ForeignKey('User', models.DO_NOTHING, related_name='followed_user')
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     class Meta:
         managed = True
@@ -45,7 +57,7 @@ class Follow(models.Model):
 class Like(models.Model):
     liker = models.ForeignKey('User', models.DO_NOTHING)
     liked = models.ForeignKey('Post', models.DO_NOTHING)
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     class Meta:
         managed = True
@@ -55,9 +67,12 @@ class Like(models.Model):
 class Log(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField()
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     owner = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
 
+    def __str__(self):
+        return self.content
+    
     class Meta:
         managed = True
         db_table = 'log'
@@ -67,8 +82,11 @@ class Message(models.Model):
     sender = models.ForeignKey('User', models.DO_NOTHING, related_name='sender_user')
     received = models.ForeignKey('User', models.DO_NOTHING, related_name='received_user')
     content = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
+    def __str__(self):
+        return self.content
+    
     class Meta:
         managed = True
         db_table = 'message'
@@ -78,9 +96,12 @@ class Notification(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField()
     is_read = models.BooleanField(blank=True, null=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     receiver = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
 
+    def __str__(self):
+        return self.content
+    
     class Meta:
         managed = True
         db_table = 'notification'
@@ -91,6 +112,9 @@ class Option(models.Model):
     name = models.CharField(blank=True, null=True, max_length=200)
     value = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         managed = True
         db_table = 'option'
@@ -100,9 +124,24 @@ class Permission(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(blank=True, null=True, max_length=200)
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         managed = True
         db_table = 'permission'
+
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(blank=True, null=True, max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        managed = True
+        db_table = 'tag'
 
 
 class Post(models.Model):
@@ -111,48 +150,31 @@ class Post(models.Model):
     content = models.TextField(blank=True, null=True)
     content_html = models.TextField(blank=True, null=True)
     read_count = models.IntegerField(blank=True, null=True)
-    created = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(blank=True, null=True, auto_now_add=True) # default=timezone.now
     author = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
+    
+    tags = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.title
+    
     class Meta:
         managed = True
         db_table = 'post'
-
-
-class PostsTags(models.Model):
-    post = models.ForeignKey(Post, models.DO_NOTHING, blank=True, null=True)
-    tag = models.ForeignKey('Tag', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = True
-        db_table = 'posts_tags'
 
 
 class Role(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(blank=True, null=True, max_length=200)
 
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return self.name
+    
     class Meta:
         managed = True
         db_table = 'role'
-
-
-class RolesPermissions(models.Model):
-    role = models.ForeignKey(Role, models.DO_NOTHING, blank=True, null=True)
-    permission = models.ForeignKey(Permission, models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = True
-        db_table = 'roles_permissions'
-
-
-class Tag(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(blank=True, null=True, max_length=200)
-
-    class Meta:
-        managed = True
-        db_table = 'tag'
 
 
 class User(models.Model):
@@ -160,14 +182,17 @@ class User(models.Model):
     name = models.CharField(blank=True, null=True, max_length=200)
     username = models.CharField(blank=True, null=True, max_length=200)
     password = models.CharField(blank=True, null=True, max_length=200)
-    mail = models.CharField(blank=True, null=True, max_length=200)
-    url = models.CharField(blank=True, null=True, max_length=200)
+    mail = models.EmailField(blank=True, null=True, max_length=200)
+    url = models.URLField(blank=True, null=True, max_length=200)
     location = models.CharField(blank=True, null=True, max_length=200)
     description = models.CharField(blank=True, null=True, max_length=200)
     last_login = models.DateTimeField(blank=True, null=True)
-    created = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     role = models.ForeignKey(Role, models.DO_NOTHING, blank=True, null=True)
-
+    
+    def __str__(self):
+        return self.username
+        
     class Meta:
         managed = True
         db_table = 'user'
